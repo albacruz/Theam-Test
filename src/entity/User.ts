d8f006d490b754a2dd2f72897338d6ee95358728
Alba Cruz Torres <albacruztorres@gmail.com>
2020-10-03 12:29:45 +0100

    User model/entity added

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -6,23 +6,11 @@ export class User {
   id: number;
 
   @Column()
-  name: string;
+  username: string;
 
   @Column()
-  surname: string;
+  password: string;
 
   @Column()
-  photo: string;
+  salt: string;
 }
-
-/*console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);*/
