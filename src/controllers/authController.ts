9080b9b5e6b149de8f717272a70aa0ecf2fa1042
Alba Cruz Torres <albacruztorres@gmail.com>
2020-10-04 19:15:55 +0100

    Sending access token when login working

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,26 @@
 import { User } from "../entity/User";
 import { getConnection } from "typeorm";
+import * as jwt from "jsonwebtoken";
+
+const accessTokenSecret = "0123456789";
 
 export async function loginUser(req, res) {
-  console.log("hello");
+  const { username, password } = req.body;
+
+  const user = await getConnection().manager.findOne(User, {
+    where: { username: username, password: password },
+  });
+
+  if (user) {
+    const accesToken = jwt.sign(
+      { id: user.id, username: user.username, role: user.role },
+      accessTokenSecret
+    );
+
+    res.json({
+      accesToken,
+    });
+  } else {
+    res.send("Username or password incorrect");
+  }
 }
